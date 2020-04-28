ecf11ea329681b5674f694a563205a0ee2891390
shessel <shessel@users.noreply.github.com>
2020-04-28 21:25:27 +0200

    exercise 03.03 bean counting

---
+++ ch03/ex03-bean-counting.js
@@ -0,0 +1,14 @@
+function countChar(s, c) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === c) count++;
+  }
+  return count;
+}
+
+function countBs(s) {
+  return countChar(s, "B");
+}
+
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
