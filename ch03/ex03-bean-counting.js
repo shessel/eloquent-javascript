f2a4df22cccb8258a729e5293030babaf719cc34
shessel <shessel@users.noreply.github.com>
2020-04-28 21:46:57 +0200

    add for-let-of version to exercise 03.03

--- ch03/ex03-bean-counting.js
+++ ch03/ex03-bean-counting.js
@@ -12,3 +12,18 @@ function countBs(s) {
 
 console.log(countBs("BBC"));
 console.log(countChar("kakkerlak", "k"));
+
+function countCharLetOf(s, c) {
+  let count = 0;
+  for (let cs of s) {
+    if (cs === c) count++;
+  }
+  return count;
+}
+
+function countBsLetOf(s) {
+  return countCharLetOf(s, "B");
+}
+
+console.log(countBsLetOf("BBC"));
+console.log(countCharLetOf("kakkerlak", "k"));
