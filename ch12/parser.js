c6ca9f51c51d59ff9377921b0d03706db25c1fdb
shessel <shessel@users.noreply.github.com>
2020-05-11 14:16:52 +0200

    add implementation for programming language project from chapter 12

---
+++ ch12/parser.js
@@ -0,0 +1,179 @@
+function parseExpression(program) {
+  program = skip(program);
+  let match, expression;
+  if (match = /^\d+\b/.exec(program)) {
+    expression = {
+      type: "value",
+      value: Number(match[0]),
+    };
+  } else if (match = /^"([^"]*)"/.exec(program)) {
+    expression = {
+      type: "value",
+      value: match[1],
+    }
+  } else if (match = /^[^(),\s"#]+/.exec(program)) {
+    expression = {
+      type: "word",
+      value: match[0],
+    }
+  } else {
+    throw new SyntaxError(`Syntax error at: ${program}`);
+  }
+
+  return parseApplicationExpression(expression, program.slice(match[0].length));
+}
+
+function parseApplicationExpression(expression, program) {
+  program = skip(program);
+  if (program[0] != "(") {
+    return {expression, program};
+  }
+
+  program = skip(program.slice(1));
+  let args = [];
+  while (program[0] != ")") {
+    let {expression: e, program: p} = parseExpression(program);
+    args.push(e);
+    program = skip(p);
+    if (program[0] == ",") {
+      program = skip(p.slice(1));
+      if (program[0] == ")") {
+        throw new SyntaxError(`expected expression after ',' found ')': ${program}`);
+      }
+    } else if (program[0] != ")") {
+      throw new SyntaxError(`expected ',' or ')', found: ${program}`);
+    }
+  }
+  return parseApplicationExpression({
+    type: "apply",
+    operator: expression,
+    args,
+  }, program.slice(1));
+}
+
+function skip(str) {
+  const skipRegExp = /^\s+/g;
+  let skipRes = skipRegExp.exec(str);
+  return str.slice(skipRegExp.lastIndex);
+}
+
+function parse(program) {
+  let {expression, program: rest} = parseExpression(program);
+  if (skip(rest).length > 0) throw new SyntaxError(`Found invalid input at the end of program: ${rest}`);
+  return expression;
+}
+
+specialForms = Object.create(null);
+
+specialForms.do = function(args, scope) {
+  for (let arg of args) {
+    evaluateExpression(arg, scope);
+  }
+};
+
+specialForms.define = function(args, scope) {
+  if (args.length != 2) throw new Error("Wrong number of arguments for define");
+  if (args[0].type == "word") {
+    value = evaluateExpression(args[1], scope)
+    scope[args[0].value] = value;
+    return value;
+  }
+};
+
+specialForms.fun = function(args, scope) {
+  if (args.length < 1) throw new SyntaxError("Can't define function without body");
+  let body = args[args.length - 1];
+  let parameters = args.slice(0, args.length-1).map(arg => {
+    if (arg.type != "word") throw new SyntaxError(`Parameter name ${arg} is not type word`);
+    return arg.value;
+  });
+
+  return function(...arguments) {
+    if (arguments.length != parameters.length) throw new Error("Invalid number of arguments for function");
+    let funScope = Object.create(scope);
+    for (let i = 0; i < parameters.length; i++) {
+      funScope[parameters[i]] = arguments[i];
+    }
+    return evaluateExpression(body, funScope);
+  } 
+}
+
+specialForms.if = function(args, scope) {
+  if (args.length != 3) throw new Error("Wrong number of arguments for if");
+  if (evaluateExpression(args[0], scope)) return evaluateExpression(args[1], scope);
+  else return evaluateExpression(args[2], scope);
+};
+
+specialForms.while = function(args, scope) {
+  if (args.length != 2) throw new Error("Wrong number of arguments for while");
+  while (evaluateExpression(args[0], scope)) {
+    evaluateExpression(args[1], scope);
+  }
+};
+
+function evaluateExpression(expr, scope) {
+  if (expr.type == "value") return expr.value;
+  else if (expr.type == "word") {
+    if (expr.value in scope) return scope[expr.value];
+    throw new Error(`Runtime error: no declaration found for ${expr.value}`);
+  } else if (expr.type == "apply") {
+    let {operator: op, args} = expr;
+    if (op.type == "word" && op.value in specialForms) {
+      return specialForms[op.value](args, scope);
+    } else {
+      let fun = evaluateExpression(expr.operator, scope);
+      if (typeof fun == "function") {
+        return fun(...args.map(arg => evaluateExpression(arg, scope)));
+      } else {
+        throw new Error("Can't apply non-function.");
+      }
+    }
+  }
+}
+
+let rootScope = Object.create(null);
+for (let op of ["+", "-", "*", "%", "<", "<=", ">", ">+", "==", "!=",]) {
+  rootScope[op] = new Function("a, b", `return a ${op} b`);
+}
+rootScope.print = console.log;
+rootScope.false = false;
+rootScope.true = true;
+
+function run(program) {
+  return evaluateExpression(parse(program), Object.create(rootScope));
+}
+
+run(`
+do(
+  print(+(4, 5)),
+  print(+(3, 5)),
+  print(+(2, 5)),
+  print(+(1, 5))
+)
+`);
+
+run(`
+do(define(total, 0),
+   define(count, 1),
+   while(<(count, 11),
+         do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+   print(total))
+`);
+
+run(`if(true, false, true)`);
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
+// → 11
+
+run(`
+do(define(pow, fun(base, exp,
+     if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+   print(pow(2, 10)))
+`);
+// → 1024
